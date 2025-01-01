ac4d1329efd0972b28d783d72d0d894000cdefe3
shubham <shubhamkannojiya45@gmail.com>
2025-01-01 23:14:01 +0530

    changes made

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ const PORT = process.env.PORT || 3500;
 app.use(
   cors({
     origin: [
-      "https://entnt-frontend-five.vercel.app",
       "https://entnt-frontend-a01758bh9-shubh2.vercel.app", // Add the correct URL here
       "http://localhost:5173",
     ], // Valid frontend URLs
